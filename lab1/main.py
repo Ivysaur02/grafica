55006aca66dab85d3f34c6b680aa25c2e59c2810
Ivysaur02 <ig.sochnev@mail.ru>
2023-09-22 22:52:29 +0400

    change create ellipse

--- lab1/main.py
+++ lab1/main.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
-colors = ['red', 'green', 'blue', 'yellow', 'pink','purple' ]
+colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
 current_index = 0
+
 
 def change_color(event):
     global current_index
@@ -13,22 +14,29 @@ def change_color(event):
     c.itemconfig(ball, fill=next_color)
 
 
-def hello():
-    print('hello')
-
 root = Tk()
-c = Canvas(width=300, height=300, bg='white')
+c = Canvas(width=500, height=500, bg='white')
 c.focus_set()
 c.pack()
 
-ball = c.create_oval(140, 140, 160, 160, fill='red')
-c.bind('<Up>', lambda event: c.move(ball, 0, -2))
-c.bind('<Down>', lambda event: c.move(ball, 0, 2))
-c.bind('<Left>', lambda event: c.move(ball, -2, 0))
-c.bind('<Right>', lambda event: c.move(ball, 2, 0))
+x = int(input("Enter the x-coordinate of the center: "))
+y = int(input("Enter the y-coordinate of the center: "))
+a = int(input("Enter the semi-axis a: "))
+b = int(input("Enter the semi-axis b: "))
+
+x0 = x - a
+y0 = y - b
+x1 = x + a
+y1 = y + b
+
+ball = c.create_oval(x0, y0, x1, y1, fill='red')
+
+c.bind('<Up>', lambda event: c.move(ball, 0, -(a / 2)))
+c.bind('<Down>', lambda event: c.move(ball, 0, (a / 2)))
+c.bind('<Left>', lambda event: c.move(ball, -(b / 2), 0))
+c.bind('<Right>', lambda event: c.move(ball, (b / 2), 0))
 c.bind('<Next>', change_color)
 c.bind('<Prior>', change_color)
 root.bind("<Escape>", lambda event: root.destroy())
-
 
 root.mainloop()
