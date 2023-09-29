97b5005523f170509ba98442cba2d02305fc8bbd
Ivysaur02 <ig.sochnev@mail.ru>
2023-09-29 21:59:52 +0400

    ready to release
    new metod create because i can test this fucking shit. new test to test
    this shit. dockfile ready to undestand this shit

--- lab1/main.py
+++ lab1/main.py
@@ -1,42 +1,111 @@
 from tkinter import *
 
-colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-current_index = 0
+
+def create_oval(canvas, x, y, a, b, color):
+    """
+    Создает овальную фигуру на холсте с заданными координатами и цветом.
+
+    Аргументы:
+        canvas (Canvas): Холст, на котором создается овальная фигура.
+        x (int): X-координата центра овала.
+        y (int): Y-координата центра овала.
+        a (int): Полуось a овала.
+        b (int): Полуось b овала.
+        color (str): Цвет заливки овала.
+
+    Возвращает:
+        int: Идентификатор нового объекта овала на холсте.
+    """
+    x0 = x - a
+    y0 = y - b
+    x1 = x + a
+    y1 = y + b
+    return canvas.create_oval(x0, y0, x1, y1, fill=color)
 
 
-def change_color(event):
-    global current_index
-    if event.keysym == 'Next':
-        current_index = (current_index + 1) % len(colors)
-    elif event.keysym == 'Prior':
-        current_index = (current_index - 1) % len(colors)
-    next_color = colors[current_index]
-    c.itemconfig(ball, fill=next_color)
+def move_oval(canvas, oval, direction, distance):
+    """
+    Перемещает овальную фигуру на холсте в заданном направлении.
+
+    Аргументы:
+        canvas (Canvas): Холст, на котором находится овальная фигура.
+        oval (int): Идентификатор объекта овала на холсте.
+        direction (str): Направление, в котором нужно переместить овальную фигуру ("up", "down", "left" или "right").
+        distance (int): Расстояние, на которое нужно переместить овальную фигуру.
+    """
+    if direction == "up":
+        canvas.move(oval, 0, -distance)
+    elif direction == "down":
+        canvas.move(oval, 0, distance)
+    elif direction == "left":
+        canvas.move(oval, -distance, 0)
+    elif direction == "right":
+        canvas.move(oval, distance, 0)
 
 
-root = Tk()
-c = Canvas(width=500, height=500, bg='white')
-c.focus_set()
-c.pack()
+def bind_keys(canvas, oval, colors, root, a, b):
+    """
+    Привязывает клавиши со стрелками, "Next", "Prior" и "Escape" к соответствующим функциям.
 
-x = int(input("Enter the x-coordinate of the center: "))
-y = int(input("Enter the y-coordinate of the center: "))
-a = int(input("Enter the semi-axis a: "))
-b = int(input("Enter the semi-axis b: "))
+    Аргументы:
+        canvas (Canvas): Холст, на котором находится овальная фигура.
+        oval (int): Идентификатор объекта овала на холсте.
+        colors (list): Список цветов заливки для овала.
+        root (Tk): Корневое окно приложения.
+        a (int): Полуось a овала.
+        b (int): Полуось b овала.
+    """
+    current_index = 0
 
-x0 = x - a
-y0 = y - b
-x1 = x + a
-y1 = y + b
+    def change_color(event):
+        nonlocal current_index
+        if event.keysym == 'Next':
+            current_index = (current_index + 1) % len(colors)
+        elif event.keysym == 'Prior':
+            current_index = (current_index - 1) % len(colors)
+        next_color = colors[current_index]
+        canvas.itemconfig(oval, fill=next_color)
 
-ball = c.create_oval(x0, y0, x1, y1, fill='red')
+    canvas.bind('<Up>', lambda event: move_oval(canvas, oval, "up", a / 2))
+    canvas.bind('<Down>', lambda event: move_oval(canvas, oval, "down", a / 2))
+    canvas.bind('<Left>', lambda event: move_oval(canvas, oval, "left", b / 2))
+    canvas.bind('<Right>', lambda event: move_oval(canvas, oval, "right", b / 2))
+    canvas.bind('<Next>', change_color)
+    canvas.bind('<Prior>', change_color)
+    canvas.bind("<Escape>", lambda event: root.destroy())
 
-c.bind('<Up>', lambda event: c.move(ball, 0, -(a / 2)))
-c.bind('<Down>', lambda event: c.move(ball, 0, (a / 2)))
-c.bind('<Left>', lambda event: c.move(ball, -(b / 2), 0))
-c.bind('<Right>', lambda event: c.move(ball, (b / 2), 0))
-c.bind('<Next>', change_color)
-c.bind('<Prior>', change_color)
-root.bind("<Escape>", lambda event: root.destroy())
 
-root.mainloop()
+def get_oval_coordinates():
+    """
+    Запрашивает у пользователя координаты и полуоси овала и возвращает их в виде кортежа.
+
+    Возвращает:
+        tuple: X-координата, Y-координата, полуось a и полуось b овала.
+    """
+    x = int(input("Введите X-координату центра: "))
+    y = int(input("Введите Y-координату центра: "))
+    a = int(input("Введите полуось a: "))
+    b = int(input("Введите полуось b: "))
+    return x, y, a, b
+
+
+def main():
+    """
+    Инициализирует холст, создает овальную фигуру, привязывает клавиши и запускает главный цикл.
+    """
+    colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+    x, y, a, b = get_oval_coordinates()
+
+    root = Tk()
+    canvas = Canvas(width=500, height=500, bg='white')
+    canvas.focus_set()
+    canvas.pack()
+
+    oval = create_oval(canvas, x, y, a, b, colors[0])
+    bind_keys(canvas, oval, colors, root, a, b)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
--- lab1/simplyTest.py
+++ lab1/simplyTest.py
@@ -1,10 +1,31 @@
 import unittest
+from tkinter import *
+from main import create_oval, move_oval
 
+class TestOval(unittest.TestCase):
+    def setUp(self):
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width=500, height=500, bg='white')
+        self.canvas.pack()
+        self.oval = create_oval(self.canvas, 250, 250, 50, 25, 'red')
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+    def test_create_oval(self):
+        self.assertEqual(self.canvas.type(self.oval), 'oval')
+        self.assertEqual(self.canvas.coords(self.oval), [200, 225, 300, 275])
+        self.assertEqual(self.canvas.itemcget(self.oval, 'fill'), 'red')
 
+    def test_move_oval(self):
+        move_oval(self.canvas, self.oval, 'up', 10)
+        self.assertEqual(self.canvas.coords(self.oval), [200, 215, 300, 265])
+        move_oval(self.canvas, self.oval, 'down', 10)
+        self.assertEqual(self.canvas.coords(self.oval), [200, 225, 300, 275])
+        move_oval(self.canvas, self.oval, 'left', 10)
+        self.assertEqual(self.canvas.coords(self.oval), [190, 225, 290, 275])
+        move_oval(self.canvas, self.oval, 'right', 10)
+        self.assertEqual(self.canvas.coords(self.oval), [200, 225, 300, 275])
+
+    def tearDown(self):
+        self.root.destroy()
 
 if __name__ == '__main__':
     unittest.main()
